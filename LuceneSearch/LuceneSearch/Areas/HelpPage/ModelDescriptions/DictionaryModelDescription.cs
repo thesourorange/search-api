63ccb512bd7efe4f1d1794ef8bed458c1e98ce9a
Neil Brittliff <nebrittl@microsoft.com>
2019-03-04 15:45:17 +1100

    First Commit

---
+++ LuceneSearch/LuceneSearch/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LuceneSearch.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ LuceneSearch/LuceneSearch/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LuceneSearch.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
